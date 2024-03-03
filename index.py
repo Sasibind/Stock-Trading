604af0da8506eb0afd154f9076c55fb2780009a6
sasibind <b.vijayasasibind@gmail.com>
2024-03-04 00:53:26 +0530

    able to add data to database now

--- index.py
+++ index.py
@@ -1,12 +1,27 @@
 import psycopg2
+import os
+import pandas as pd
+from sqlalchemy import create_engine
 
-#establishing the connection
+db_params = {
+   'host': '127.0.0.1',
+   "port" : '5432',
+   'database': 'postgres',
+   'user': 'postgres',
+   'password': 'password'
+}
+
+# establishing the connection
 conn = psycopg2.connect(
-   database="postgres", user='postgres', password='password', host='127.0.0.1', port= '5432'
+   database=db_params['database'],
+   user=db_params['user'],
+   password=db_params['password'],
+   host=db_params['host'],
+   port=db_params['port']
 )
 conn.autocommit = True
 
-#Creating a cursor object using the cursor() method
+# creating a cursor object using the cursor() method
 cursor = conn.cursor()
 
 # deleting old data and creating a new database
@@ -16,5 +31,34 @@ sql = '''CREATE database stock_data;'''
 cursor.execute(sql)
 # print("new database created")
 
-#Closing the connection
+# connecting to the newly database
+db_params['database'] = 'stock_data'
+engine = create_engine(f'postgresql://{db_params["user"]}:{db_params["password"]}@{db_params["host"]}/{db_params["database"]}')
+
+# defining the file paths for the data files
+dirpath = os.path.join(os.getcwd(), 'data')
+file_list = os.listdir(dirpath)
+
+file_loc = {}
+
+# mapping table names to filepaths
+for file in file_list:
+   filepath = os.path.join(dirpath, file)
+   name = file[:-4].lower()
+   name = ''.join(e for e in name if e.isalnum())
+   file_loc[name] = filepath
+
+# # printing some data from the tables to verify
+# for filename, filepath in file_loc.items():
+#    print(f"Contents of '{filename}' CSV file:")
+#    df = pd.read_csv(filepath)
+#    print(df.head(2))
+#    print("\n")
+
+# adding the data to the database
+for filename, filepath in file_loc.items():
+   df = pd.read_csv(filepath)
+   df.to_sql(filename, engine, if_exists='replace', index=False)
+
+# closing the connection
 conn.close()
